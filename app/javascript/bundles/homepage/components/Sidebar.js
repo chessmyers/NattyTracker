30a03649eddb72a1d7c9674894e22d1ac0ebca49
scheung97 <scheung1997@gmail.com>
2021-03-23 19:10:59 -0400

    use database to create burger menu checkbox items

--- app/javascript/bundles/homepage/components/Sidebar.js
+++ app/javascript/bundles/homepage/components/Sidebar.js
@@ -1,64 +1,39 @@
 import React from 'react';
 import { slide as Menu } from 'react-burger-menu';
 
+//creates burger menu checkbox 
+function ParkItems(props){
+  return <a href = {"/parks/" + props.id}>
+  <input type = "checkbox" className="menu-item"></input>
+  <label htmlFor="menu-item"> {props.name} </label>
+  <br/> <br/>
+  </a>; 
+}
+
 export default props => {
-  var parks = props.parks; 
-  // var arrayLength = props.parks.length;
-  for (var i = 0; i < parks.length; i++) {
-    console.log(props.parks[i]['name']);
-    //Do something
+  // const parks = props.parks
+
+  //use first 62 IDs (reduces size by ~95%)
+  var parks = []; 
+  for(var i = 0; i < 61; i++){ //should probably remove hard coding
+    parks[i] = props.parks[i]; 
+    if(i == 28)
+      parks[i].name = "Haleakala National Park"; //last "a" in parks[28].name is a special character; will tackle this issue later
   }
 
+  //parkItems = react elements for each iteration of parks
+  const parkItems = parks.map((park) => 
+    <ParkItems key={park.id.toString()} id={park.id} name={park.name}/> 
+  );
+  
   return (
-    <Menu>
+    <Menu> 
       <a className="menu-item" href="/homepage">
         Home
       </a>
       <br/>
 
-
-      {/* I think this would be how we get all the parks to show? */}
-
-       {/* {arr.map((parks) => { 
-        return (
-        { <a href = {"/parks/" + parks.id}>
-          <input type = "checkbox" className="menu-item"></input>
-          <label for="menu-item"> {parks.name} </label>
-          <br/>
-        </a>
-        )
-      })} */}
-
-
-      <a href = "/parks/1">
-        <input type = "checkbox" className="menu-item"></input>
-        <label htmlFor="menu-item"> Arcadia </label>
-      </a> 
-      <br/>
-      <a href = "/parks/2">
-        <input type = "checkbox" className="menu-item"></input>
-        <label htmlFor="menu-item"> Arches </label>
-      </a> 
-      <br/>
-      <a href = "/parks/3">
-        <input type = "checkbox" className="menu-item"></input>
-        <label htmlFor="menu-item"> Badlands </label>
-      </a>
-      <br/>
-      <a href = "/parks/2">
-        <input type = "checkbox" className="menu-item"></input>
-        <label htmlFor="menu-item"> Big Bend </label>
-      </a>
-      <br/>
-      <a href = "/parks/2">
-        <input type = "checkbox" className="menu-item"></input>
-        <label htmlFor="menu-item"> Grand Canyon </label>
-      </a>
-      <br/>
-      <a href = "/parks/2">
-        <input type = "checkbox" className="menu-item"></input>
-        <label htmlFor="menu-item"> Yosemite </label>
-      </a>
+      {parkItems} {/* yay */}
     </Menu>
   );
 };
